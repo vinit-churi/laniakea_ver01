cb60a5ac823235519fd75d6b5302df2a6cce7bda
Vidyesh Churi <vidyesh95@gmail.com>
2023-12-16 21:11:46 +0530

    Update FadeOutCollection styling

--- components/FadeOutCollection.tsx
+++ components/FadeOutCollection.tsx
@@ -4,9 +4,9 @@ import React from "react";
 const FadeOutCollection = () => {
   return (
     <div
-      className="before:contents-[''] before:h-full before:w-28 before:absolute before:left-0 before:top-0 before:bg-gradient-to-r before:from-white before:from-30%  before:to-transparent before:to-90%  after:contents-[''] after:h-full after:w-28 after:absolute
-    after:right-0 after:top-0 after:bg-gradient-to-l after:from-white after:from-30%  after:to-transparent after:to-90% 
-    relative   mx-auto overflow-hidden flex justify-center gap-8 w-[90%] max-w-[1024px] my-[128px]"
+      className="before:contents-[''] before:h-full before:w-32 before:absolute before:left-0 before:top-0 before:bg-gradient-to-r before:from-white before:from-30% before:via-white/70 before:via-60% before:to-transparent before:to-100% 
+                 after:contents-[''] after:h-full after:w-32 after:absolute after:right-0 after:top-0 after:bg-gradient-to-l after:from-white after:from-30% after:via-white/70 after:via-60% after:to-transparent after:to-100% 
+    relative   mx-auto overflow-hidden flex justify-center gap-8 w-[100%] max-w-[1114px] my-[128px]"
     >
       <Image
         height={387}
