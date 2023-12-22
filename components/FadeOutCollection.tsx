c3cae791a007483fcd2bedb23d65b95c31ae8460
Vinit Churi <vinitchuri@hotmail.com>
2023-12-22 15:28:50 +0530

    Refactor FadeOutCollection component

--- components/FadeOutCollection.tsx
+++ components/FadeOutCollection.tsx
@@ -3,32 +3,31 @@ import React from "react";
 
 const FadeOutCollection = () => {
   return (
-    <div
-      className="before:contents-[''] before:h-full before:w-32 before:absolute before:left-0 before:top-0 before:bg-gradient-to-r before:from-white before:from-30% before:via-white/70 before:via-60% before:to-transparent before:to-100% 
-               after:contents-[''] after:h-full after:w-32 after:absolute after:right-0 after:top-0 after:bg-gradient-to-l after:from-white after:from-30% after:via-white/70 after:via-60% after:to-transparent after:to-100% 
-    relative mx-auto overflow-hidden flex justify-center gap-8 w-[100%] max-w-[1114px] my-[128px]"
-    >
-      <Image
-        height={387}
-        width={688}
-        src="/images/fade_out_collection.png"
-        alt="collection"
-        className="rounded-2xl w-[688px] h-[387px] flex-1 shrink-0"
-      />
-      <Image
-        height={387}
-        width={688}
-        src="/images/fade_out_collection.png"
-        alt="collection"
-        className="rounded-2xl w-[688px] h-[387px] flex-1 shrink-0"
-      />
-      <Image
-        height={387}
-        width={688}
-        src="/images/fade_out_collection.png"
-        alt="collection"
-        className="rounded-2xl w-[688px] h-[387px] flex-1 shrink-0"
-      />
+    <div className="before:contents-[''] after:contents-[''] relative mx-auto my-[128px] flex w-[100%] max-w-[1114px]  justify-center gap-8 overflow-hidden before:absolute before:left-0 before:top-0 before:z-10 before:h-full before:w-32 before:bg-gradient-to-r before:from-white before:from-30% before:via-white/70 before:via-60% before:to-transparent before:to-100% after:absolute after:right-0 after:top-0 after:h-full after:w-32 after:bg-gradient-to-l after:from-white after:from-30% after:via-white/70 after:via-60% after:to-transparent after:to-100% max-[500px]:gap-4">
+      <div className="relative aspect-[16/9] w-[688px]  shrink-0 max-[900px]:w-[500px] max-[715px]:w-[300px] max-[500px]:w-[200px]">
+        <Image
+          fill
+          src="/images/fade_out_collection.png"
+          alt="collection"
+          className="cover rounded-2xl object-cover object-center"
+        />
+      </div>
+      <div className="relative aspect-[16/9] w-[688px]   shrink-0 max-[900px]:w-[500px] max-[715px]:w-[300px] max-[500px]:w-[200px]">
+        <Image
+          fill
+          src="/images/fade_out_collection.png"
+          alt="collection"
+          className="cover rounded-2xl object-cover object-center"
+        />
+      </div>
+      <div className="relative aspect-[16/9] w-[688px]  shrink-0 max-[900px]:w-[500px] max-[715px]:w-[300px] max-[500px]:w-[200px]">
+        <Image
+          fill
+          src="/images/fade_out_collection.png"
+          alt="collection"
+          className="cover rounded-2xl object-cover object-center"
+        />
+      </div>
     </div>
   );
 };
--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-// prettier.config.js
-module.exports = {
-  singleQuote: true,
-  tabWidth: 4,
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindConfig: "./tailwind.config.ts",
-};
