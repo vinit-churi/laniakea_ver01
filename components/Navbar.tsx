591055dc004d8f6d0b901d9c301bbf966225b14d
Vinit Churi <vinitchuri@hotmail.com>
2024-01-16 21:14:44 +0530

    navbar nav items partially done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,188 +18,179 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 import MobileNav from "./MobileNav";
 import AppDownloadPrompt from "./Hero/AppDownloadPrompt";
+import { usePathname } from "next/navigation";
 
 const components: { title: string; href: string; description: string }[] = [
   {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
+    title: "Featured Items",
+    href: "/",
+    description: "Explore our curated collection of exquisite jewelry pieces.",
   },
   {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
+    title: "New Arrivals",
+    href: "/",
     description:
-      "For sighted users to preview content available behind a link.",
+      "Discover the latest additions to our stunning jewelry selection.",
   },
   {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+    title: "Collections",
+    href: "/",
+    description: "Browse through our exclusive jewelry collections.",
   },
   {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
+    title: "Special Offers",
+    href: "/",
+    description:
+      "Find amazing deals and discounts on our high-quality jewelry.",
   },
   {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
+    title: "Gift Ideas",
+    href: "/",
     description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+      "Explore thoughtful and elegant jewelry options for special occasions.",
   },
   {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
+    title: "All Categories",
+    href: "/all-categories",
     description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+      "Explore rest of the categories and find the best products for you.",
   },
 ];
 
 function Navbar() {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
-    <>
+    <div className="">
       <AppDownloadPrompt />
-      <div className="mx-auto grid w-[90%] max-w-[1024px] grid-cols-[max-content_auto_max-content] grid-rows-[max-content_max-content] items-center py-4 max-[1060px]:hidden">
-        <Image
-          src="/images/Laniakea.svg"
-          alt="logo"
-          width={133}
-          height={40}
-          className="relative z-10 cursor-pointer"
-        />
-        <NavigationMenu className="pl-[32px]">
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className="bg-transparent text-white hover:bg-transparent hover:text-white">
-                Themed Jewelry
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                  {components.map((component) => (
-                    <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
-                    >
-                      {component.description}
-                    </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className="bg-transparent text-white hover:bg-transparent hover:text-white">
-                All Categories
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                  <li className="row-span-3">
-                    <NavigationMenuLink asChild>
-                      <a
-                        className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                        href="/"
-                      >
-                        {/* <Icons.logo className="h-6 w-6" /> */}
-                        <div className="mb-2 mt-4 text-lg font-medium">
-                          shadcn/ui
-                        </div>
-                        <p className="text-sm leading-tight text-muted-foreground">
-                          Beautifully designed components built with Radix UI
-                          and Tailwind CSS.
-                        </p>
-                      </a>
-                    </NavigationMenuLink>
-                  </li>
-                  <ListItem href="/docs" title="Introduction">
-                    Re-usable components built using Radix UI and Tailwind CSS.
-                  </ListItem>
-                  <ListItem href="/docs/installation" title="Installation">
-                    How to install dependencies and structure your app.
-                  </ListItem>
-                  <ListItem
-                    href="/docs/primitives/typography"
-                    title="Typography"
-                  >
-                    Styles for headings, paragraphs, lists...etc
-                  </ListItem>
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className="bg-transparent text-white hover:bg-transparent hover:text-white">
-                Customized
-              </NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                  {components.map((component) => (
-                    <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
-                    >
-                      {component.description}
-                    </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/docs" legacyBehavior passHref>
-                <NavigationMenuLink
-                  className={`${navigationMenuTriggerStyle()} bg-transparent text-white hover:bg-transparent hover:text-white`}
-                >
-                  Explore
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/docs" legacyBehavior passHref>
-                <NavigationMenuLink
-                  className={`${navigationMenuTriggerStyle()} bg-transparent text-white hover:bg-transparent hover:text-white`}
-                >
-                  Support
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
-        <div className=" relative z-10 flex items-center gap-5 ">
-          <Image
-            className="cursor-pointer"
-            src="/images/search.svg"
-            height={24}
-            width={24}
-            alt="search"
-          />
-          <Image
-            className="cursor-pointer"
-            src="/images/local_mall.svg"
-            height={24}
-            width={24}
-            alt="mall"
-          />
-          <Image
-            className="cursor-pointer"
-            src="/images/web.svg"
-            height={24}
-            width={24}
-            alt="web"
-          />
-          <Button
-            className=" flex h-[44px] w-[115px]  items-center justify-center gap-2 rounded-full bg-primaryRegular text-white transition-[colors_scale] duration-300 ease-in-out hover:scale-105 hover:bg-primaryRegular/80 hover:text-white"
-            variant="ghost"
-          >
+      <div
+        className={`${pathname === "/all-categories" ? "bg-[#3E0500]" : ""}`}
+      >
+        <div
+          className={`mx-auto grid w-[90%] max-w-[1024px] grid-cols-[max-content_auto_max-content] grid-rows-[max-content_max-content] items-center py-4 max-[1060px]:hidden `}
+        >
+          <Link href="/">
             <Image
-              src="/images/person.svg"
+              src="/images/Laniakea.svg"
+              alt="logo"
+              width={133}
+              height={40}
+              className="relative z-10 cursor-pointer"
+            />
+          </Link>
+          <NavigationMenu className="pl-[32px]">
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className="bg-transparent text-white hover:bg-transparent hover:text-white">
+                  Explore
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                    {components.map((component) => (
+                      <ListItem
+                        key={component.title}
+                        title={component.title}
+                        href={component.href}
+                      >
+                        {component.description}
+                      </ListItem>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger className="bg-transparent text-white hover:bg-transparent hover:text-white">
+                  All Categories
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="flex h-max w-max gap-4 p-4">
+                    <li className="h-[300px] w-[300px]">
+                      <NavigationMenuLink asChild>
+                        <Link
+                          className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                          href="/"
+                        >
+                          {/* <Icons.logo className="h-6 w-6" /> */}
+                          <div className="mb-2 mt-4 text-lg font-medium">
+                            shadcn/ui
+                          </div>
+                          <p className="text-sm leading-tight text-muted-foreground">
+                            Beautifully designed components built with Radix UI
+                            and Tailwind CSS.
+                          </p>
+                        </Link>
+                      </NavigationMenuLink>
+                    </li>
+                    <li className="w-[600px] self-stretch bg-purple-50">
+                      temp container
+                    </li>
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink
+                    className={`${navigationMenuTriggerStyle()} bg-transparent text-white hover:bg-transparent hover:text-white`}
+                  >
+                    Themed jewelry
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink
+                    className={`${navigationMenuTriggerStyle()} bg-transparent text-white hover:bg-transparent hover:text-white`}
+                  >
+                    Customized
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink
+                    className={`${navigationMenuTriggerStyle()} bg-transparent text-white hover:bg-transparent hover:text-white`}
+                  >
+                    Support
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <div className=" relative z-10 flex items-center gap-5 ">
+            <Image
+              className="cursor-pointer"
+              src="/images/search.svg"
               height={24}
               width={24}
-              alt="person"
+              alt="search"
             />
-            Sign in
-          </Button>
+            <Image
+              className="cursor-pointer"
+              src="/images/local_mall.svg"
+              height={24}
+              width={24}
+              alt="mall"
+            />
+            <Image
+              className="cursor-pointer"
+              src="/images/web.svg"
+              height={24}
+              width={24}
+              alt="web"
+            />
+            <Button
+              className=" flex h-[44px] w-[115px]  items-center justify-center gap-2 rounded-full bg-primaryRegular text-white transition-[colors_scale] duration-300 ease-in-out hover:scale-105 hover:bg-primaryRegular/80 hover:text-white"
+              variant="ghost"
+            >
+              <Image
+                src="/images/person.svg"
+                height={24}
+                width={24}
+                alt="person"
+              />
+              Sign in
+            </Button>
+          </div>
         </div>
       </div>
       <div className="mx-auto hidden w-[100%] max-w-[1024px] grid-cols-[max-content_auto_max-content]  items-center py-4 max-[1060px]:grid">
@@ -227,7 +218,7 @@ function Navbar() {
           className="relative z-50 m-4 cursor-pointer"
         />
       </div>
-    </>
+    </div>
   );
 }
 
