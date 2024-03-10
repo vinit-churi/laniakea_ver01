1f21d2b770de612f03d0197a4f183b534001b901
Vinit Churi <vinitchuri@hotmail.com>
2024-03-10 17:33:26 +0530

    Add AllProductsContainer to page component

--- app/(home)/all-products/[[...slug]]/page.tsx
+++ app/(home)/all-products/[[...slug]]/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import AllProductsContainer from "@/components/AllProducts/AllProductsContainer";
 const page = () => {
-  return <div></div>;
+  return (
+    <div>
+      <AllProductsContainer />
+    </div>
+  );
 };
 
 export default page;
---
+++ components/AllProducts/AllProductsContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Product from "@/components/AllProducts/Product";
+const AllProductsContainer = () => {
+  return (
+    <div>
+      <p>All products container</p>
+      <Product />
+    </div>
+  );
+};
+
+export default AllProductsContainer;
